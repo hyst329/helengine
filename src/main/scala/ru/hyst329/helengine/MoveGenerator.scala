673655d82f9ddba53a808a6097377a3bfea07453
hyst329 <trolleybus.1329@gmail.com>
2019-07-16 13:24:42 +0300

    added more code (move generation), some refactoring (less magic numbers)

--- src/main/scala/ru/hyst329/helengine/MoveGenerator.scala
+++ src/main/scala/ru/hyst329/helengine/MoveGenerator.scala
@@ -15,21 +15,48 @@ object MoveGenerator {
     val occupationAll      = board.occupationAll
     AllSquares.foreach { square =>
       board.getPiece(square.toByte) match {
-        case pawn =>
+        case `pawn` =>
         // Generate pawn moves
-        case knight =>
+        // TODO: Pawn moves
+        case `knight` =>
           val mask = MagicBitBoards.KingPatterns(square) & ~occupationCurrent
-          result +:= AllSquares.filter(s => (mask & 1L << s) != 0).map(dest => Move.fromBoardContext(board, square.toByte, dest.toByte))
-        case bishop =>
-        // Generate bishop moves
-        case rook =>
-        // Generate rook moves
-        case queen =>
+          result ++= AllSquares
+            .filter(s => (mask & 1L << s) != 0)
+            .map(dest => Move.fromBoardContext(board, square.toByte, dest.toByte))
+        case `bishop` =>
+          // Generate bishop moves
+          val mask: BitBoard = MagicBitBoards.BishopAttackTable(square)((
+            ((occupationAll | MagicBitBoards.BishopMasks(square))
+              * MagicBitBoards.BishopMagic(square)) >>> (64 - MagicBitBoards.BishopBits(square))).toInt)
+          result ++= AllSquares
+            .filter(s => (mask & 1L << s) != 0)
+            .map(dest => Move.fromBoardContext(board, square.toByte, dest.toByte))
+        case `rook` =>
+          // Generate rook moves
+          val mask: BitBoard = MagicBitBoards.RookAttackTable(square)((
+            ((occupationAll | MagicBitBoards.RookMasks(square))
+              * MagicBitBoards.RookMagic(square)) >>> (64 - MagicBitBoards.RookBits(square))).toInt)
+          result ++= AllSquares
+            .filter(s => (mask & 1L << s) != 0)
+            .map(dest => Move.fromBoardContext(board, square.toByte, dest.toByte))
+        case `queen` =>
+          val maskBishop: BitBoard = MagicBitBoards.BishopAttackTable(square)((
+            ((occupationAll | MagicBitBoards.BishopMasks(square))
+              * MagicBitBoards.BishopMagic(square)) >>> (64 - MagicBitBoards.BishopBits(square))).toInt)
+          val maskRook: BitBoard = MagicBitBoards.RookAttackTable(square)((
+            ((occupationAll | MagicBitBoards.RookMasks(square))
+              * MagicBitBoards.RookMagic(square)) >>> (64 - MagicBitBoards.RookBits(square))).toInt)
+          val mask = maskBishop | maskRook
+          result ++= AllSquares
+            .filter(s => (mask & 1L << s) != 0)
+            .map(dest => Move.fromBoardContext(board, square.toByte, dest.toByte))
         // Generate queen moves
-        case king =>
-        // Generate king moves
+        case `king` =>
+          // Generate king moves
           val mask = MagicBitBoards.KingPatterns(square) & ~occupationCurrent
-          result +:= AllSquares.filter(s => (mask & 1L << s) != 0).map(dest => Move.fromBoardContext(board, square.toByte, dest.toByte))
+          result ++= AllSquares
+            .filter(s => (mask & 1L << s) != 0)
+            .map(dest => Move.fromBoardContext(board, square.toByte, dest.toByte))
         case _ =>
         // Do nothing, either an opponent's piece or empty square
       }
